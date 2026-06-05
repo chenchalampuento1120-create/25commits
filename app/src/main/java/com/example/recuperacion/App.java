22dfda2f48a3db3979e8b01a26a1da586f825bd9
Chen Chalampuento <chen.chalampuento1120.utc.edu.ec>
2026-06-04 22:05:57 -0500

    cambios chen 7

--- app/src/main/java/com/example/recuperacion/App.java
+++ app/src/main/java/com/example/recuperacion/App.java
@@ -13,6 +13,8 @@ public class App {
         System.out.println("-----------------------------");
         System.out.println("Ingrese su nota referente al primer parcial: ");
         double n1 =sc.nextDouble();
+        System.out.println("ingrese la nota del segundo parcial: ");
+        double n2 =sc.nextDouble();
     }
         
     }
